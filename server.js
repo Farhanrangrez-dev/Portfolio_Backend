d6be923534b56c3e2903ac6a74f2694b3fb44160
Farhan Rangrez <farhanragrez@gmail.com>
2026-03-23 15:16:44 +0530

    server changse

--- server.js
+++ server.js
@@ -21,9 +21,17 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/' // Railway ke liye MUST
+}));
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'upload')));
