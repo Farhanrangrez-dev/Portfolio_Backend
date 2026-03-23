17a63c79398cdbcfe81869c0086bd42e12f8297a
Farhan Rangrez <farhanragrez@gmail.com>
2026-03-23 17:39:22 +0530

    project Update router changse

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -5,7 +5,7 @@ const upload = require("../middleware/upload");
 const { getProjects, createProject, updateProject, deleteProject, getCategories,getSingleProject } = require("../controllers/projectController");
 
 router.route("/").get(getProjects).post(protect, upload.single("image"), createProject);
-router.route("/:id").put(protect, updateProject).delete(protect, deleteProject);
+router.route("/projects/:id").put(upload.single("image"), updateProject).delete(protect, deleteProject);
 router.get("/categories", getCategories)
 router.get("/single/:id", getSingleProject)
 
